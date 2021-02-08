49b49d7efc725aed08f7215ae80b8bf2ee3bce80
Way-Yuhao <liu697@wisc.edu>
2021-02-08 15:32:08 -0600

    added a second dataset

--- evaluate.py
+++ evaluate.py
@@ -12,8 +12,7 @@ from vgg_perceptual_loss import VGGPerceptualLoss
 from tqdm import tqdm
 from tabulate import tabulate
 
-cmos_path = "./data/CMOS"
-gt_path = "./data/ground_truth"
+"""GLOBAL PARAMETERS"""
 _mini_batch_size = 16
 
 
@@ -63,52 +62,93 @@ def compute_vgg_loss(output, target):
 
 
 def main():
+    cmos_path = "./data/CMOS"
+    gt_path = "./data/ground_truth"
+    fusion_path = "./data/SPAD_HDR_SR"
+    ds_names = np.array(["cmos ldr", "luminance fusion"])
+    loss_func_names = ["l1 loss", "content loss", "vgg16 loss"]
+
     set_device()
     torch.cuda.empty_cache()
     transform = set_transform()
 
-    ds_names = np.array(["cmos ldr"])
-    loss_func_names = ["l1 loss", "content loss", "vgg16 loss"]
-
     # initialize data loaders
     cmos_data_loader = load_data("png", cmos_path, transform)
     gt_data_loader = load_data("hdr", gt_path, transform)
+    fusion_data_loader = load_data("hdr", fusion_path, transform)
+    data_loaders = [gt_data_loader, cmos_data_loader, fusion_data_loader]
 
     # check if data sets match in size
-    assert(len(cmos_data_loader.dataset) == len(gt_data_loader.dataset))
-    num_mini_batches = len(cmos_data_loader)
-    ds_size = len(cmos_data_loader.dataset)
+    for i in range(1, len(data_loaders)):
+        assert (len(data_loaders[i].dataset) == len(data_loaders[0].dataset))
+
+    num_mini_batches = len(data_loaders[0])
+    ds_size = len(data_loaders[0].dataset)
+    print("metrics: ", loss_func_names)
+    print("avaliable data sets: ", ds_names)
     print("size of dataset = {}, mini-batch size = {}".format(ds_size, _mini_batch_size))
 
-    cmos_it = iter(cmos_data_loader)
-    gt_it = iter(gt_data_loader)
+    iterables = [iter(data_loaders[i]) for i in range(1, len(data_loaders))]
+    # cmos_it = iter(cmos_data_loader)
+    # gt_it = iter(gt_data_loader)
 
-    # computing loss
-    l1_loss, content_loss, vgg_loss = 0, 0, 0
     metrics = None
-    for i in tqdm(range(num_mini_batches)):
-        cmos_data, _ = cmos_it.next()
-        gt_data, _ = gt_it.next()
-        cmos_data = cmos_data.to(torch.device("cuda:0"))
-        gt_data = gt_data.to(torch.device("cuda:0"))
-        cur_mini_batch_size = len(cmos_data)
+    for i in range(len(iterables)):
+        torch.cuda.empty_cache()
+        print("evaluating data set ", ds_names[i])
+        gt_it = iter(gt_data_loader)
+        # computing loss
+        cur_metrics = np.zeros(len(loss_func_names))
+        for j in tqdm(range(num_mini_batches)):
+            output_data, _ = iterables[i].next()
+            gt_data, _ = gt_it.next()
+            output_data = output_data.to(torch.device("cuda:0"))
+            gt_data = gt_data.to(torch.device("cuda:0"))
+            cur_mini_batch_size = len(output_data)
 
-        l1_loss += compute_l1_loss(cmos_data, gt_data) * cur_mini_batch_size
-        content_loss += compute_content_loss(cmos_data, gt_data) * cur_mini_batch_size
-        vgg_loss += compute_vgg_loss(cmos_data, gt_data) * cur_mini_batch_size
-    l1_loss /= ds_size
-    content_loss /= ds_size
-    vgg_loss /= ds_size
-    cur_metrics = np.array([l1_loss, content_loss, vgg_loss])
+            l1_loss = compute_l1_loss(output_data, gt_data)
+            content_loss = compute_content_loss(output_data, gt_data)
+            torch.cuda.empty_cache()
+            # vgg_loss = compute_vgg_loss(output_data, gt_data) # TODO
+            vgg_loss = -1
+            cur_metrics = np.add(cur_metrics, cur_mini_batch_size * np.array([l1_loss, content_loss, vgg_loss]))
 
-    metrics = np.array([cur_metrics]) if metrics is None else np.vstack((metrics, cur_metrics))
-    print(metrics)
-    print(metrics.shape)
+        cur_metrics = cur_metrics / ds_size
+        metrics = np.array([cur_metrics]) if metrics is None else np.vstack((metrics, cur_metrics))
+        gt_it = None
 
-    # setting up table to print
     table_content = np.hstack((ds_names.reshape(-1, 1), metrics))
     table = tabulate(table_content, loss_func_names, tablefmt="pretty")
     print(table)
+
+
+
+    # # computing loss
+    # l1_loss, content_loss, vgg_loss = 0, 0, 0
+    # metrics = None
+    # for i in tqdm(range(num_mini_batches)):
+    #     cmos_data, _ = cmos_it.next()
+    #     gt_data, _ = gt_it.next()
+    #     cmos_data = cmos_data.to(torch.device("cuda:0"))
+    #     gt_data = gt_data.to(torch.device("cuda:0"))
+    #     cur_mini_batch_size = len(cmos_data)
+    #
+    #     l1_loss += compute_l1_loss(cmos_data, gt_data) * cur_mini_batch_size
+    #     content_loss += compute_content_loss(cmos_data, gt_data) * cur_mini_batch_size
+    #     vgg_loss += compute_vgg_loss(cmos_data, gt_data) * cur_mini_batch_size
+    # l1_loss /= ds_size
+    # content_loss /= ds_size
+    # vgg_loss /= ds_size
+    # cur_metrics = np.array([l1_loss, content_loss, vgg_loss])
+    #
+    # metrics = np.array([cur_metrics]) if metrics is None else np.vstack((metrics, cur_metrics))
+    # print(metrics)
+    # print(metrics.shape)
+
+    # setting up table to print
+    # table_content = np.hstack((ds_names.reshape(-1, 1), metrics))
+    # table = tabulate(table_content, loss_func_names, tablefmt="pretty")
+    # print(table)
 
     # print("l1 loss = ", l1_loss.item())
     # print("content loss = ", content_loss.item())
